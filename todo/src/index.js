348313ac35c138881b6e3299e8e3735c64795e5a
Melvin Peepers <peepersm@gmail.com>
2019-07-13 12:28:53 -0400

    Setup Redux on index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import createStore & Provider
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// This is the function that will take in a single reducer that will represent the state (data) of our application globally.
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// temporary till we add our reducer
+function reducer() {
+  return {
+    title: "ToDo"
+  };
+}
+
+// added <Provider> and store={store}
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
